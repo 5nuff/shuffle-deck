498c3c926c3465844e6940e2756b9ede3eddbb1d
5nuff <intechnic.l.r@gmail.com>
2019-11-07 09:17:40 -0500

    shuffle-deck

--- public/main.js
+++ public/main.js
@@ -1,7 +1,53 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+const deck = []
+
+const createDeck = () => {
+  const suit = ['Diamonds', 'Clubs', 'Spades', 'Hearts']
+  const rank = [
+    'Ace',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'Jack',
+    'Queen',
+    'King'
+  ]
+
+  // create the deck based on the suits and rank array
+  for (let i = 0; i < suit.length; i++) {
+    for (let j = 0; j < rank.length; j++) {
+      deck.push(rank[j] + ' of ' + suit[i])
+    }
+  }
+  // nested for-loop
+}
+
+let j = 0
+let i = 0
+
+const shuffleDeck = () => {
+  for (i = 52; i >= 1; i--) {
+    j = Math.floor(Math.random() * i)
+
+    const swapper = deck[j]
+    deck[j] = deck[i]
+    deck[i] = swapper
   }
 }
 
+const main = () => {
+  createDeck()
+  shuffleDeck()
+}
+
+const clickDeck = () => {
+  document.querySelector('.face-up').textContent = deck.pop()
+}
+
 document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.face-down').addEventListener('click', clickDeck)
